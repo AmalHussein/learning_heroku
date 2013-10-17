dcf50b8fdbe6932aa731f666a600228eeb1ace0e
Theodore Cleveland <tcclevela@gmail.com>
2013-10-17 11:47:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearningHeroku::Application.config.secret_key_base = '3a5ba4eb4b5a67a9fb79e21011ba67bffeb8be2c90d7e1cd4c9439553818edaf64a30412cd273c0e489cb61e2901a409e9fc145e536aabaf1b61f3a69f4ddbaa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LearningHeroku::Application.config.session_store :cookie_store, key: '_learning_heroku_session'
